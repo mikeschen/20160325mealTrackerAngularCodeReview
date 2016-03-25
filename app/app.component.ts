c5086fa930c4c4034cdff92823431600f9e38f0c
Michael Schennum <mikeschenphoto@gmail.com>
2016-03-25 08:39:33 -0700

    adds for loop so meals can appear on home page

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,18 +3,29 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
-  </div>
+	  <div class="container">
+	    <h1>Globo Gym</h1>
+	    <h3 *ngFor="#meal of meals" (click)="mealWasSelected(meal)">
+	    	{{ meal.name }}
+	    </h3>
+	  </div>
   `
 })
 
 export class AppComponent { 
-
+	public meal: Meal;
+	constructor() {
+		this.meals = [
+			this.meal = new Meal("Pizza", "I ate an entire medium pizza", 900, 0),
+			this.meal = new Meal("Hamburger", "With no bun, lettuce, or cheese", 300, 0),
+			this.meal = new Meal("Hotdog", "I threw it on the ground", 0, 0),
+			this.meal = new Meal("Fries", "I ate one fry", 15, 0)
+		]
+	}
 }
 
 export class Meal {
-	constructor(public food: string, public description: string, public calories: number, public id: number) {
+	constructor(public name: string, public description: string, public calories: number, public id: number) {
 
 	}
 }
